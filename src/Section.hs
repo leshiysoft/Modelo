0305065507a856520393623e63a0ddb1617ec817
leshiysoft <leshiysoft@yandex.ru>
2020-01-23 12:58:15 +0300

    Section.hs

---
+++ src/Section.hs
@@ -0,0 +1,26 @@
+module Section where
+
+import Point
+import Vector
+import Bezier
+
+data Section = Section {
+  sectionStart :: Point,
+  sectionEnd :: Point}
+  deriving (Eq, Show)
+
+sectionMiddle :: Section -> Point
+sectionMiddle s = vectorPoint $ times 0.5 (vStart + vEnd)
+  where
+    vStart = Vector $ sectionStart s
+    vEnd = Vector $ sectionEnd s
+
+sectionToVector :: Section -> Vector
+sectionToVector s = vectorBeginEnd (sectionStart s) (sectionEnd s)
+
+sectionToBezier :: Section -> Bezier
+sectionToBezier sec@(Section s e) = Bezier (s, v1) (e, v2)
+  where
+    theVec = sectionToVector sec
+    v1 = times (1/3) theVec
+    v2 = times (1/3) (-theVec)
