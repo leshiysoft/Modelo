eb56c49238ce2d3861318c8bf75bf1c0687fb45a
leshiysoft <leshiysoft@yandex.ru>
2020-03-03 17:10:25 +0300

    Заготовка для объекта описывающего поверхность

---
+++ src/Surface.hs
@@ -0,0 +1,21 @@
+module Surface where
+
+import Value
+import Point
+import Mesh
+import Shape
+
+data Surface = Surface (Value -> Value -> Point)
+
+surfaceFunction :: Surface -> (Value -> Value -> Point)
+surfaceFunction (Surface sf) = sf
+
+--TODO: реализовать конвертацию в Mesh
+-- surfaceToMesh :: Int -> Int -> Surface -> Mesh
+-- surfaceToMesh i j
+
+class Surfaceable s where
+  toSurface :: s -> Surface
+
+instance Shape Surface where
+  move v (Surface sf) = Surface (\v1 v2 -> move v $ sf v1 v2)
